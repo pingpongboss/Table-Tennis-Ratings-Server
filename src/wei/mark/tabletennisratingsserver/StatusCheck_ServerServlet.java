92e9520a63945368f903f5fd8140d55b64f1b560
Mark Wei <markwei@gmail.com>
2011-04-10 02:38:16 -0700

    new libs. v6

---
+++ src/wei/mark/tabletennisratingsserver/StatusCheck_ServerServlet.java
@@ -0,0 +1,30 @@
+package wei.mark.tabletennisratingsserver;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import wei.mark.tabletennisratingsserver.util.EMF;
+
+@SuppressWarnings("serial")
+public class StatusCheck_ServerServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String response = null;
+
+		try {
+			EMF.get().createEntityManager();
+			response = "OK";
+		} catch (Exception ex) {
+			log(ex.getMessage());
+			response = "ERROR";
+		}
+
+		resp.setContentType("text/plain");
+		resp.getWriter().println(response);
+	}
+}
