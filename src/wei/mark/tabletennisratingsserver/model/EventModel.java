264d602c4995e8a1a5c9b86b58c10251f1a6d791
Mark Wei <markwei@gmail.com>
2011-05-15 21:58:54 -0700

    EventModel

---
+++ src/wei/mark/tabletennisratingsserver/model/EventModel.java
@@ -0,0 +1,112 @@
+package wei.mark.tabletennisratingsserver.model;
+
+import javax.persistence.Id;
+
+import com.googlecode.objectify.annotation.Cached;
+import com.googlecode.objectify.annotation.Unindexed;
+
+@Cached
+public class EventModel {
+	@Id
+	Long key;
+
+	String playerId;
+	String id;
+	String name;
+
+	@Unindexed
+	String date;
+	@Unindexed
+	String ratingBefore;
+	@Unindexed
+	String ratingChange;
+	@Unindexed
+	String ratingAfter;
+	@Unindexed
+	String matches;
+	@Unindexed
+	String record;
+
+	public EventModel() {
+	}
+
+	public Long getKey() {
+		return key;
+	}
+
+	public void setKey(Long key) {
+		this.key = key;
+	}
+
+	public String getPlayerId() {
+		return playerId;
+	}
+
+	public void setPlayerId(String playerId) {
+		this.playerId = playerId;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getRatingBefore() {
+		return ratingBefore;
+	}
+
+	public void setRatingBefore(String ratingBefore) {
+		this.ratingBefore = ratingBefore;
+	}
+
+	public String getRatingChange() {
+		return ratingChange;
+	}
+
+	public void setRatingChange(String ratingChange) {
+		this.ratingChange = ratingChange;
+	}
+
+	public String getRatingAfter() {
+		return ratingAfter;
+	}
+
+	public void setRatingAfter(String ratingAfter) {
+		this.ratingAfter = ratingAfter;
+	}
+
+	public String getMatches() {
+		return matches;
+	}
+
+	public void setMatches(String matches) {
+		this.matches = matches;
+	}
+
+	public String getRecord() {
+		return record;
+	}
+
+	public void setRecord(String record) {
+		this.record = record;
+	}
+}
