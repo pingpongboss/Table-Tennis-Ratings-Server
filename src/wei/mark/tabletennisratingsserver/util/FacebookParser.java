28d0752855ee14c830540f704dcb55affcfcbe03
Mark Wei <markwei@gmail.com>
2011-12-10 23:56:59 -0800

    add self to friends

--- src/wei/mark/tabletennisratingsserver/util/FacebookParser.java
+++ src/wei/mark/tabletennisratingsserver/util/FacebookParser.java
@@ -13,10 +13,48 @@ import com.google.appengine.repackaged.org.json.JSONObject;
 
 public class FacebookParser {
 	public static final String GRAPH_PATH_BASE = "https://graph.facebook.com/";
+	public static final String GRAPH_PATH_PART_SELF = "?access_token=";
 	public static final String GRAPH_PATH_PART_FRIENDS = "/friends?access_token=";
 
 	public static ArrayList<FriendModel> getFriends(String facebookId,
 			String accessToken, boolean linked) {
+		ArrayList<FriendModel> friendsArray = new ArrayList<FriendModel>();
+		DAO dao = new DAO();
+
+		// Add self if either not linked only or if self has already been linked
+		if (!linked || dao.existsPlayer(facebookId)) {
+			HttpURLConnection connection = null;
+			try {
+				URL url = new URL(GRAPH_PATH_BASE + facebookId
+						+ GRAPH_PATH_PART_SELF + accessToken);
+
+				connection = (HttpURLConnection) url.openConnection();
+
+				BufferedReader rd = new BufferedReader(new InputStreamReader(
+						connection.getInputStream(), "UTF-8"));
+
+				StringBuilder sb = new StringBuilder();
+				String line;
+				while ((line = rd.readLine()) != null) {
+					sb.append(line);
+				}
+				rd.close();
+
+				JSONObject data = new JSONObject(sb.toString());
+
+				FriendModel self = new FriendModel(facebookId,
+						data.getString("name"));
+
+				friendsArray.add(self);
+			} catch (Exception e) {
+				return null;
+			} finally {
+				if (connection != null)
+					connection.disconnect();
+			}
+		}
+
+		// add rest of friends
 		HttpURLConnection connection = null;
 		try {
 			URL url = new URL(GRAPH_PATH_BASE + facebookId
@@ -34,28 +72,21 @@ public class FacebookParser {
 			}
 			rd.close();
 
-			ArrayList<FriendModel> friendsArray = new ArrayList<FriendModel>();
-
 			JSONObject data = new JSONObject(sb.toString());
 			JSONArray friends = data.getJSONArray("data");
 			for (int i = 0; i < friends.length(); i++) {
-				JSONObject friend = friends.getJSONObject(i);
-				friendsArray.add(new FriendModel(friend.getString("id"), friend
-						.getString("name")));
+				JSONObject friendJson = friends.getJSONObject(i);
+
+				FriendModel friend = new FriendModel(
+						friendJson.getString("id"),
+						friendJson.getString("name"));
+
+				if (!linked || dao.existsPlayer(friend.getId())) {
+					friendsArray.add(friend);
+				}
 			}
 
-			if (linked) {
-				ArrayList<FriendModel> linkedFriends = new ArrayList<FriendModel>();
-				DAO dao = new DAO();
-				for (FriendModel friend : friendsArray) {
-					if (dao.existsPlayer(friend.getId())) {
-						linkedFriends.add(friend);
-					}
-				}
-				return linkedFriends;
-			} else {
-				return friendsArray;
-			}
+			return friendsArray;
 		} catch (Exception e) {
 			return null;
 		} finally {
